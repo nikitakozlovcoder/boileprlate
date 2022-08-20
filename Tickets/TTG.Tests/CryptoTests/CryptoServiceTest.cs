6186c6bbbd6bbd2507bc54333b6d7c83fb963dd1
Nikita Kozlov <nikita@thetourguy.com>
2022-08-20 13:05:24 +0300

    unit tests

---
+++ Tickets/TTG.Tests/CryptoTests/CryptoServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moonshine.Core.Security.Cryptography.Contracts;
+using Moonshine.Core.Security.Cryptography.KeyStore.Contracts;
+using Moonshine.Core.Security.Cryptography.KeyStore.Dto;
+using MoonshineTest.Moqs;
+using Moq;
+using Xunit;
+
+namespace MoonshineTest.Services.CryptoTests;
+
+public class CryptoServiceTests : BaseTest
+{
+    private readonly ICryptoService _cryptoService;
+
+    public CryptoServiceTest()
+    {
+        AddMoq<IAsymmetricKeyStoreProvider>(moq =>
+        {
+            moq.Setup(x => x.GetKeyInfo(It.IsAny<CancellationToken>())).ReturnsAsync(new AsymmetricKeyStoreValue
+            {
+                PrivateKey = AsymmetricKeyStoreValuesMoq.PrivateKey,
+                PublicKey = AsymmetricKeyStoreValuesMoq.PublicKey
+            });
+        });
+       
+        _cryptoService = ServiceProvider.GetRequiredService<ICryptoService>();
+    }
+
+    [Theory]
+    [InlineData( "test text to test encrypt and decrypt")]
+    public async Task Encrypt_And_Decrypt_Success(string testText)
+    {
+        var textAfterEncryption = await _cryptoService.Encrypt(testText, default);
+        Assert.NotEmpty(textAfterEncryption);
+        
+        var textAfterDecryption = await _cryptoService.Decrypt(textAfterEncryption, default);
+        Assert.NotEmpty(textAfterDecryption);
+        Assert.Equal(textAfterDecryption, testText);
+    }
+    
+    [Theory]
+    [InlineData( "text to alter")]
+    public async Task Encrypt_And_Decrypt_Fails(string alterText)
+    {
+        var alteredBytes = Encoding.UTF8.GetBytes(alterText);
+        var task = _cryptoService.Decrypt(alteredBytes, default);
+        await Assert.ThrowsAnyAsync<Exception>(async () => await task);
+    }
+}
---
+++ Tickets/TTG.Tests/CryptoTests/SignatureServiceTest.cs
@@ -0,0 +1,53 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Moonshine.Core.Security.Cryptography.Contracts;
+using Moonshine.Core.Security.Cryptography.KeyStore.Contracts;
+using Moonshine.Core.Security.Cryptography.KeyStore.Dto;
+using MoonshineTest.Moqs;
+using Moq;
+using Xunit;
+
+namespace MoonshineTest.Services.CryptoTests;
+
+public class SignatureServiceTests : BaseTest
+{
+    private readonly ISignatureService _signatureService;
+
+    public SignatureServiceTest()
+    {
+        AddMoq<IAsymmetricKeyStoreProvider>(moq =>
+        {
+            moq.Setup(x => x.GetKeyInfo(It.IsAny<CancellationToken>())).ReturnsAsync(new AsymmetricKeyStoreValue
+            {
+                PrivateKey = AsymmetricKeyStoreValuesMoq.PrivateKey,
+                PublicKey = AsymmetricKeyStoreValuesMoq.PublicKey
+            });
+        });
+        
+        _signatureService = ServiceProvider.GetRequiredService<ISignatureService>();
+
+    }
+
+    [Theory]
+    [InlineData( "test text to test signature")]
+    public async Task Sign_And_Verify_Success(string testText)
+    {
+        var signature = await _signatureService.Sign(testText, default);
+        Assert.NotEmpty(signature);
+        
+        var result = await _signatureService.Verify(testText, signature, default);
+        Assert.True(result);
+    }
+    
+    [Theory]
+    [InlineData( "test text to test signature", "altered text")]
+    public async Task Sign_And_Verify_Fails(string testText, string alteredText)
+    {
+        var signature = await _signatureService.Sign(testText, default);
+        Assert.NotEmpty(signature);
+        
+        var result = await _signatureService.Verify(alteredText, signature, default);
+        Assert.False(result);
+    }
+}
